ba82d003cd967e1ad2b598d980c7cdb51d053de1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-11 17:38:59 +0700

    update query jadwal praktikum

--- sql, csv dan table view/query_jadwal_praktikum.sql
+++ sql, csv dan table view/query_jadwal_praktikum.sql
@@ -3,7 +3,7 @@ CREATE VIEW `query_jadwal_praktikum` AS
         `ruang_praktikum`.`ruang`,
         `jadwal_praktikum`.`hari`,
         `sesi_praktikum`.`kelas`,
-        `sesi_praktikum`.`modul`,
+        `sesi_praktikum`.`mata_praktikum`,
         `sesi_praktikum`.`sesi`
     FROM
         `ruang_praktikum`
@@ -13,4 +13,4 @@ CREATE VIEW `query_jadwal_praktikum` AS
         `sesi_praktikum` ON `ruang_praktikum`.`id_ruang_praktikum` = `sesi_praktikum`.`id_ruang_praktikum`
     WHERE
         `sesi_praktikum`.`kelas` IS NOT NULL
-            AND `sesi_praktikum`.`modul` IS NOT NULL
+            AND `sesi_praktikum`.`mata_praktikum` IS NOT NULL
